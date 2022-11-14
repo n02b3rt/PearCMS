98ab8dd73799bb8872491b1203adc770600de7b0
Norbert Fxxxx <theonionman627@gmail.com>
2022-11-14 22:40:03 +0100

    dodalem strone glowna

---
+++ components/home/header.php
@@ -0,0 +1,9 @@
+<header class="home__header">
+
+    <img src="<?php echo $_SESSION['logo']?>" alt="Login logo figure" class="logobox__image">
+
+    <h1 class="logobox__caption">
+        <span class="logobox__caption--main">Pear</span></br>
+        Content Management System
+    </h1>
+</header>
--- index.php
+++ index.php
@@ -5,23 +5,37 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CMS</title>
     <link rel="stylesheet" href="Css/style.css">
-    <link rel="shortcut icon" href="Img/base/favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,300;0,400;0,600;1,400;1,600&display=swap"
         rel="stylesheet">
+
+    <?php 
+        if(!isset($_SESSION['title']) && !isset($_SESSION['favicon']))
+            include 'scripts/php/base_session_set.php';
+    ?>
+
+    <title><?php echo $_SESSION['title']?></title>
+    <link rel="shortcut icon" href="<?php echo $_SESSION['favicon']?>" />
+
 </head>
 
 <body>
     <?php include 'scripts/php/data_base_con.php'; ?>
     <!-- content -->
-    <?php include 'components/header.php'; ?>
+    <main class="home">
+        <?php  include 'components/home/header.php'?>
+
+        <!-- <?php include 'components/home/content.php`'?> -->
+
+
+    </main>
+
     <!--  -->
 
-    <?php include 'components/starter.php'?>
+    <!-- <?php include 'components/starter.php'?> -->
 </body>
 
 </html>
