e39d888e023238cff287122c47c0e30a64f9c6a4
Norbert Fxxxx <theonionman627@gmail.com>
2022-11-24 20:15:25 +0100

    ostatni push cms - zmiana konceptu - robimy bardziej portal socialmedia typu twitter

--- components/dashboard/add-entry.php
+++ components/dashboard/add-entry.php
@@ -1,11 +1,29 @@
 <!-- TODO ogarnąć formularz + walidacja go -->
+<?php
+$msg = "";
+$target = "img/".basename($_FILES['image']['name']);
+
+$db = mysqli_connect('localhost','root','','cms');
+
+$image = $_FILES['image']['name'];
+$sql = "INSERT INTO `test`(`test`) VALUES ('$image')";
+mysqli_query($db,$sql);
+
+if(move_uploaded_file($_FILES['image']['tmp_name'],$target)){
+    $msg = "GIT";
+}
+else {
+    $msg = "NIEGIT";
+}
+?>
 <div class="add-entry__container">
-    <form action="#" method="post">
+    <form action="dashboard.php?add-entries" method="post">
         <span class="caption">Add-entry</span>
         <input type="text" name="title" id=title placeholder="Wpisz tytuł wpisu">
         <div class="side-bar">
             <div class="photo__preview"></div>
-            <input type="file" name="photo" class="photo" accept=".jpg, .jpeg, .png, .webp">
+             <input class="form-control" type="file" name="image" value="" />
+            <!-- <input type="file" name="photo" class="photo" accept=".jpg, .jpeg, .png, .webp"> -->
             <select name="categories" id="categories">
                 <option value="Politics">Politics</option>
                 <option value="World">World</option>
---
+++ scripts/php/dashboard_add-entry.php
@@ -0,0 +1,18 @@
+<?php
+    /*
+    title
+photo
+categories
+content
+    */ 
+    // if(isset($_POST['title']) && isset($_POST['photo']) && isset($_POST['categories']) && isset($_POST['content']) ){
+    //     $image_file = $_FILES["photo"];
+    //     move_uploaded_file($image_file['tmp_name'],__DIR__ . "/images/" . $image_file["name"]);
+    //     echo $photo;
+    //     // $connect = mysqli_connect('localhost','root','','cms');
+    //     // echo $_POST['title'],"<img src='$photo'>",$_POST['categories'],$_POST['content'];
+    // }
+    // error_reporting(0);
+// If upload button is clicked ...
+
+?>
